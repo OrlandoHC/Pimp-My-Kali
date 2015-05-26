12043712bebb66b092fe5539e78ed6e800438844
Ing. Orlando Hernandez Cruz (OrlandoHC/Masterk3y) <orlando.hdzcruz@gmail.com>
2015-05-25 23:39:35 -0500

    Update 1.1 Beta
    Nueva Versión Disponible Con Muchas Mejoras.
    LOG Actualizado

--- pmk.sh
+++ pmk.sh
@@ -3,23 +3,38 @@ if [[ $EUID -ne 0 ]]; then
 echo "Este script debe ser ejecutado por el usuario Root" 1>&2
 exit 1
 fi
-# LISTA DE CAMBIOS
-# Versión 1.0 beta last update 22/03/2015
+# LISTA DE CAMBIOS 
+# Version 1.1 Beta (25/05/15)
+#-MODS
+#	- Ejecucion de Si/No Insensible a Mayusculas o Minusculas.
+#	- Condicion para seleccion de Arquitectura x86/x64.
+#	- Se creo un repositporio para las apps dedicadas (Minidwep,Feeding Bootle y Sublime Text).
+#	- Se Movieron los antiguos archivos numix.zip y elementary al nuevo repositorio.
+#	- Gkrellm ha sido movido al nuevo repositorio.
+#	- Enlace al repositorio ---> http://orlandohc.ddns.net/repo
+#	- Leyenda para cancelacion de reinicio presionando CTRL + C.
+#-APPS
+#	- MinidWep
+#	- Feeding Bootle
+#	- Sublime Text
+#	- File Roller
+#
+# Versión 1.0 beta (22/03/15)
 #-MODS
 #	- Condición de Ejecución restrictiva para usuarios Root.
 #	- Comprobación de Conexion a Internet, En caso de no contar con conexión el script aborta.
 #	- Al finalizar, Pimp My Kali reinicia el sistema en 1 minuto para que surtan efecto las modificaciones.
 #	- Dentro del Banner se agrego mi pagina web www.orlandohc.org y el @OrlandoHC esto con fines informativos y didácticos.
-#	- Marcas Sleep para facil comprensión del usuario acerca proceso que se lleva a cabo en tiempo real. 
+#	- Marcas Sleep para facil comprensión del usuario acerca proceso que se lleva a cabo en tiempo real.
 #-APPS
-#	- Se Eliminó Open Office de la lista de Apps, esto para disminuir el tiempo, se tiene pensado incluir Evernote.
+#	- Se Eliminó Open Office de la lista de Apps, esto para disminuir el tiempo.
 #
 # Versión 1.1 alpha
 #-MODS
 #	- Interfaz que lanza el script correspondiente para personalizar Kali Linux.
 #	- Desactivación De Cursos Parpadeante.
 #	- Instalación de GKrellM Barra de Herramientas.
-#	- Activación De Configuración Avanzada, Escritorio, Extensiones, Gnome-Shell, 
+#	- Activación De Configuración Avanzada, Escritorio, Extensiones, Gnome-Shell,
 #	Nautilus, Tema, Tipografias y Ventanas (Mejor Conocido como Gnome Tweak Tool).
 #	- Personalización Del Entorno Tema Numix e Iconos Elementary.
 #-APPS
@@ -32,7 +47,7 @@ fi
 #	- Puty
 #	- K3b
 #
-# Versión 1.1 alpha
+# Versión 1.0 alpha (Inicial - No Disponible)
 #-MODS
 #	- Mejoras estéticas.
 #	- Se cambio el nombre del dominio del server a orlandohc.ddns.net.
@@ -46,24 +61,24 @@ fi
 #	- Flash Plugin
 #
 sc(){
-echo -e " 
+echo -e "
 ############################################################
 ##                                                        ##
-##   __           __                                      ##
-##  |__) |  |\/| |__)     |\/| \ /    |__/  /\  |    |    ##
-##  |    |  |  | |        |  |  |     |  \ /~~\ |___ |    ##
+##    __           __                                     ##
+##   |__) |  |\/| |__)     |\/| \ /    |__/  /\  |    |   ##
+##   |    |  |  | |        |  |  |     |  \ /~~\ |___ |   ##
 ##                                                        ##
-##                        1.0-beta                        ##
-##               By: Masterk3y (@OrlandoHC) 	          ##
-##                    www.orlahdohc.org     	          ## 
+##                         1.1-beta                       ##
+##                By: Masterk3y (@OrlandoHC) 	          ##
+##                    www.orlahdohc.org      	          ##
 ##                                                        ##
 ############################################################
-\n"
-echo -e "\033[1;37mScript para Personalizar Kali Linux\033[0m \n\n "
-echo -e "\033[1;37mInstrucciones: S = SI   N = NO (Mayuscula)\033[0m \n\n "
-echo -e "¿Deseas Personalizar tu Kali Linux? [ \033[0;32mS\033[0m / \033[0;31mN\033[0m ]: \c ";
+"
+echo -e "\033[1;37m[- Script Automatizado para Personalizar Kali Linux x86/x64 -]\033[0m \n "
+echo -e "\033[1;37mInstrucciones: S/s = Instalar  N/n = Cancelar Instalacion \033[0m \n "
+echo -e "Deseas Personalizar tu Kali Linux? [ \033[0;32mS/s\033[0m / \033[0;31mN/n\033[0m ]: \c ";
 read respuesta
-if [ $respuesta = "S" ]; then
+if [ $(echo $respuesta | tr -s '[:upper:]' '[:lower:]') = "s" ]; then
 echo -e "\033[0;31m[+]\033[0m Comprobando Conexion a Internet...";
 sleep 3
 if [ "`ping -c 1 www.google.com`" ]
@@ -71,10 +86,10 @@ then
 	echo -e "\033[0;31m[+]\033[0m Ping Exitoso, Status = Conectado";
 	sleep 3
 	else
-	echo -e "\033[0;31m[+]\033[0m Ping Muerto, Al parecer no tienes conexion a Internet, Verifica y reintenta nuevamente."; exit 1 
+	echo -e "\033[0;31m[+]\033[0m Ping Muerto, Al parecer no tienes conexion a Internet, Verifica y reintenta nuevamente."; exit 1
 fi
 sleep 3
-echo -e "\033[0;31m[+]\033[0m Comenzando Personalización";
+echo -e "\033[0;31m[+]\033[0m Comenzando Personalizacion, No Apague ni Desconecte su Equipo";
 echo -e "\033[0;31m[+]\033[0m Deshabilitando Cursor Parpadeante";
 gsettings set org.gnome.desktop.interface cursor-blink false
 echo -e "\033[0;31m[+]\033[0m Instalando Ark ";
@@ -101,6 +116,8 @@ echo -e "\033[0;31m[+]\033[0m Instalando GTk Record My Desktop";
 apt-get install gtk-recordmydesktop recordmydesktop remmina -y
 echo -e "\033[0;31m[+]\033[0m Instalando GDeby";
 apt-get install gdebi -y
+echo -e "\033[0;31m[+]\033[0m Instalando File Roller";
+apt-get install file-roller
 echo -e "\033[0;31m[+]\033[0m Incorporando Flash Plugin";
 apt-get install apt-get flashplugin-nonfree
 echo -e "\033[0;31m[+]\033[0m Configurando Audio";
@@ -108,20 +125,20 @@ apt-get install alsa-utils -y
 echo -e "\033[0;31m[+]\033[0m Instalando GKrellM";
 apt-get install gkrellm
 echo -e "\033[0;31m[+]\033[0m Descargando Skins De GKrellM";
-wget http://orlandohc.ddns.net/downloads/GKrellM-Skins.tar.gz
+wget http://orlandohc.ddns.net/repo/gkrellm/GKrellM-Skins.tar.gz
 echo -e "\033[0;31m[+]\033[0m Descomprimiendo Skins";
 for i in $(ls *.tar.gz)
 do
 tar -xzvf $i;
 done
-for i in *.tar.gz; 
-do 
-tar -xzvf $i; 
+for i in *.tar.gz;
+do
+tar -xzvf $i;
 done
 mv GKrellM-skins/  ~/home
 echo -e "\033[0;31m[+]\033[0m Descargando Archivos Para Personalizar Entorno";
-wget http://orlandohc.ddns.net/downloads/numix.zip
-wget http://orlandohc.ddns.net/downloads/elementary.zip
+wget http://orlandohc.ddns.net/repo/gnome/numix.zip
+wget http://orlandohc.ddns.net/repo/gnome/elementary.zip
 echo -e "\033[0;31m[+]\033[0m Descomprimiendo Archivos Para Personalizar Entorno";
 for i in $(ls *.zip)
 do
@@ -130,13 +147,57 @@ done
 mv Numix/ /usr/share/themes
 mv elementary/ /usr/share/icons
 sleep 3
-echo -e $green"\033[0;31m[+]\033[0m Instalación Exitosa!";
+echo -e "\033[1;37mPara x64 Escribe = 64 / Para x86 Escribe = 86 \033[0m "
+echo -e "Cual es tu Arquitectura de Kali Linux? [ \033[0;32m64\033[0m / \033[0;31m86\033[0m ]: \c";
+read answ2
+if [ $answ2 = "64" ]; then
+echo -e "\033[0;31m[+]\033[0m Descargando Paquetes Minidwep, Feeding Bootle y Sublime Text para Kali Linux x64";
 sleep 3
-echo -e "\033[0;31m[+]\033[0m Kali Se Reiniciará para que los cambios surtan efecto. Guarda tu trabajo"
+wget http://orlandohc.ddns.net/repo/minidwep/minidwep-gtk-40420-bt5-64bit.deb
+wget http://orlandohc.ddns.net/repo/feedingbootle/feedingbottle3.2.3.deb
+wget http://orlandohc.ddns.net/repo/sublimetext/sublime-text_build-3047_amd64.deb
+echo -e "\033[0;31m[+]\033[0m Descomprimiendo Minidwep...";
+sleep 3
+dpkg -i minidwep-gtk-40420-bt5-64bit.deb
+sleep 3
+echo -e "\033[0;31m[+]\033[0m Descomprimiendo FeedingBootle...";
+sleep 3
+dpkg -i feedingbottle3.2.3.deb
+sleep 3
+echo -e "\033[0;31m[+]\033[0m Descomprimiendo Sublime Text...";
+sleep 3
+dpkg -i sublime-text_build-3047_amd64.deb
+echo -e "\033[0;31m[+]\033[0m Paquetes Instalados Correctamente."
+fi
+sleep 3
+if [ $answ2 = "86" ]; then
+echo -e "\033[0;31m[+]\033[0m Descargando Paquetes Minidwep, Feeding Bootle y Sublime Text para Kali Linux x86";
+sleep 3
+wget http://orlandohc.ddns.net/repo/minidwep/minidwep-gtk-40420-bt5-32bit.deb
+wget http://orlandohc.ddns.net/repo/feedingbootle/feedingbottle3.2.3.deb
+wget http://orlandohc.ddns.net/repo/sublimetext/sublime-text_build-3047_i386.deb
+echo -e "\033[0;31m[+]\033[0m Descomprimiendo Minidwep...";
+sleep 3
+dpkg -i minidwep-gtk-40420-bt5-32bit.deb
+sleep 3
+echo -e "\033[0;31m[+]\033[0m Descomprimiendo FeedingBootle...";
+sleep 3
+dpkg -i feedingbottle3.2.3.deb
+echo -e "\033[0;31m[+]\033[0m Descomprimiendo Sublime Text...";
+sleep 3
+dpkg -i sublime-text_build-3047_i386.deb
+sleep 3
+echo -e "\033[0;31m[+]\033[0m Paquetes Instalados Correctamente."
+fi
+sleep 3
+echo -e $green"\033[0;31m[+]\033[0m Instalacion de Pimp My Kali Exitosa!";
+sleep 3
+echo -e "\033[0;31m[+]\033[0m Kali Se Reiniciara para que los cambios surtan efecto. Guarda tu trabajo!"
+echo -e "\033[0;31m[+]\033[0m Para Cancelar El Reinicio presiona: CTRL + C"
 shutdown -r +1
 fi
-if [ $respuesta = "N" ]; then
-echo -e "\033[0;31m \n[-] Operación Cancelada [-]\033[0m";
+if [ $(echo $respuesta | tr -s '[:upper:]' '[:lower:]') = "n" ]; then
+echo -e "\033[0;31m \n[-] Operacion Cancelada [-]\033[0m";
 fi
 }
 sc
